7fe356efcdb054761a0fdeff415845a73adda177
[Erik Bahena] <[erikjbahena@gmail.com]>
2022-03-04 18:36:21 -0800

    added first_name and last_name in the user seed

--- data/seeds/01-user.js
+++ data/seeds/01-user.js
@@ -2,6 +2,8 @@ exports.seed = function (knex) {
   return knex("user").insert([
     {
       email: "test@gmail.com",
+      first_name: "fname",
+      last_name: "lname",
       password: "test",
       user_id: 1,
     },
